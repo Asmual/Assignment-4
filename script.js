7b7b349736a2bdac7220699caef5f84e156f8bb4
Asmual <Asmual04@gmail.com>
2026-02-22 20:14:57 +0600

    Interview, Rejected, Delete Function

--- script.js
+++ script.js
@@ -15,26 +15,23 @@ const noJobs = document.getElementById("No-Jobs");
 let interviewJobs = [];
 let rejectedJobs = [];
 
-/* SetActive tab */
+
+/* SetActive Tab */
 function setActiveTab(activeButton) {
 
   const tabButtons = [allTab, interviewTab, rejectedTab];
 
   for (const btn of tabButtons) {
-    btn.classList.remove("bg-[#3B82F6]");
-    btn.classList.remove("text-white");
-    btn.classList.add("bg-gray-200");
-    btn.classList.add("text-gray-700");
+    btn.classList.remove("bg-[#3B82F6]", "text-white");
+    btn.classList.add("bg-gray-200", "text-gray-700");
   }
 
-  activeButton.classList.remove("bg-gray-200");
-  activeButton.classList.remove("text-gray-700");
-  activeButton.classList.add("bg-[#3B82F6]");
-  activeButton.classList.add("text-white");
+  activeButton.classList.remove("bg-gray-200", "text-gray-700");
+  activeButton.classList.add("bg-[#3B82F6]", "text-white");
 }
 
 
-/* Card Count */
+/* VisibleCard Count */
 function visibleCards() {
 
   let count = 0;
@@ -49,24 +46,22 @@ function visibleCards() {
   return count;
 }
 
+
 /* Dashboard Update */
 function updateDashboard() {
 
   interviewCount.innerText = interviewJobs.length;
   rejectedCount.innerText = rejectedJobs.length;
-
   const totalCards = cardContainer.children.length;
-
   totalCount.innerText = totalCards;
-
   jobsCountText.innerText = visibleCards() + " of " + totalCards + " jobs";
 }
+
 
 /* All Tab */
 allTab.addEventListener("click", function () {
 
   setActiveTab(allTab);
-
   const cards = cardContainer.children;
 
   for (const card of cards) {
@@ -74,17 +69,15 @@ allTab.addEventListener("click", function () {
   }
 
   noJobs.classList.add("hidden");
-
   updateDashboard();
 });
+
 
 /* Interview Tab */
 interviewTab.addEventListener("click", function () {
 
   setActiveTab(interviewTab);
-
   const cards = cardContainer.children;
-
   for (const card of cards) {
     if (interviewJobs.includes(card)) {
       card.style.display = "block";
@@ -102,3 +95,97 @@ interviewTab.addEventListener("click", function () {
   updateDashboard();
 });
 
+
+/* Rejected Tab */
+rejectedTab.addEventListener("click", function () {
+
+  setActiveTab(rejectedTab);
+  const cards = cardContainer.children;
+  for (const card of cards) {
+    if (rejectedJobs.includes(card)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  }
+
+  if (rejectedJobs.length === 0) {
+    noJobs.classList.remove("hidden");
+  } else {
+    noJobs.classList.add("hidden");
+  }
+
+  updateDashboard();
+});
+
+
+/* Interview, Rejected, Delete Function */
+function setupCardButtons() {
+
+  const cards = cardContainer.children;
+  for (const card of cards) {
+    const actionButtons = card.querySelectorAll(".flex button");
+    const interviewButton = actionButtons[0];
+    const rejectedButton = actionButtons[1];
+    const statusBadge = card.querySelector("span");
+    const deleteButton = card.querySelector(".fa-trash-can").parentElement;
+
+
+    /* Interview btn */
+    interviewButton.addEventListener("click", function () {
+
+      if (!interviewJobs.includes(card)) {
+        interviewJobs.push(card);
+      }
+
+      rejectedJobs = rejectedJobs.filter(function (item) {
+        return item !== card;
+      });
+
+      statusBadge.innerText = "INTERVIEW";
+      statusBadge.className =
+        "inline-block mt-3 px-3 py-2 text-xs font-semibold bg-green-200 text-green-800 rounded-md";
+
+      updateDashboard();
+    });
+
+
+    /* Rejected Button */
+    rejectedButton.addEventListener("click", function () {
+
+      if (!rejectedJobs.includes(card)) {
+        rejectedJobs.push(card);
+      }
+
+      interviewJobs = interviewJobs.filter(function (item) {
+        return item !== card;
+      });
+
+      statusBadge.innerText = "REJECTED";
+      statusBadge.className =
+        "inline-block mt-3 px-3 py-2 text-xs font-semibold bg-red-200 text-red-800 rounded-md";
+
+      updateDashboard();
+    });
+
+
+    /* Delete Button */
+    deleteButton.addEventListener("click", function () {
+
+      interviewJobs = interviewJobs.filter(function (item) {
+        return item !== card;
+      });
+
+      rejectedJobs = rejectedJobs.filter(function (item) {
+        return item !== card;
+      });
+
+      card.remove();
+      updateDashboard();
+    });
+
+  }
+}
+
+setupCardButtons();
+updateDashboard();
